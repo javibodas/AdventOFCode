06b9a5502f6538798173296646dc0a30ac5e7f59
Javier González Bodas <jgonzalez.bodas@atsistemas.com>
2020-12-11 14:57:55 +0100

    Init project. Added structure 2020/Advent11

---
+++ 2020/Advent11.py
@@ -0,0 +1,18 @@
+import sys
+_NUM_SEATS = 0
+_NUM_SEAT_ROWS = 0
+_NUM_SEAT_COLUMNS = 0
+
+
+
+
+
+def readSeats():
+    return
+
+
+
+readSeats()
+print("Num of seats: {0}".format(_NUM_SEATS))
+print("Num of seat rows: {0}".format(_NUM_SEAT_ROWS))
+print("Num of seat columns: {0}".format(_NUM_SEAT_COLUMNS))
